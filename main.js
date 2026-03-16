9a2ac174f7390e75640319020dc0106e91f8da2c
andrealoda <andrea@casaloda.it>
2026-03-16 21:12:41 +0100

    Implement task addition functionality and update UI styles

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-document.getElementById('btn').addEventListener('click',
-    function() {
-        console.log('click');
+// document.getElementById('btn').addEventListener('click',
+//     function() {
+//         console.log('click');
+//     }
+// );
+
+
+
+
+
+function taskAdder() {
+    const addTask = document.createElement('li');
+
+    const taskList = document.getElementById('task-list');
+
+    const taskInput = document.getElementById('task').value;
+    const taskText = document.createTextNode(taskInput);
+    const errorDisplay = document.querySelector('.error');
+
+    // addTask.appendChild(taskText);
+
+    // taskList.appendChild(addTask);
+
+    if (taskInput === '') {
+        errorDisplay.style.display = 'block'
+
+    } else {
+        addTask.appendChild(taskText);
+
+        taskList.appendChild(addTask);
+        errorDisplay.style.display = 'none'
+
     }
-);
+
+}
 
