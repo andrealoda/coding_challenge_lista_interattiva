134a87b117fe69fd3468d330b43f58d665c1d313
andrealoda <andrea@casaloda.it>
2026-03-22 21:57:17 +0100

    add neew style for li elements

--- main.js
+++ main.js
@@ -6,18 +6,28 @@ function taskAdder() {
     const taskList = document.getElementById('task-list');
 
     const taskInput = document.getElementById('task').value;
+    document.getElementById('task').value = '';
 
-    const taskText = document.createTextNode(taskInput);
+    // const taskText = document.createTextNode(taskInput);
 
     const errorDisplay = document.querySelector('.error');
+    
+    const divLi = document.createElement('div');
+    divLi.classList.add('task-row');
+    divLi.innerHTML = `
+        <i class="bi bi-square"></i>
+        <i class="bi bi-check-square-fill"></i>
+        <span>${taskInput}</span>
+        <i class="bi bi-trash"></i>`;
 
     if (taskInput === '') {
         errorDisplay.innerHTML = 'Non puoi aggiungere un&apos;attività vuota.';
         errorDisplay.style.display = 'block'
     } else {
-        addTask.appendChild(taskText);
+        addTask.appendChild(divLi);
         taskList.appendChild(addTask);
         errorDisplay.style.display = 'none'
+
+
     }
 }
-
